4443e672260c0082e24cf3b39d789e0aaa6de207
limingxing <limingxing@limingxingdeMacBook-Pro.local>
2015-01-10 23:31:51 +0800

    Logback intergrated.

--- torani-cap/torani-supplier/src/main/java/org/coffeehouse/torani/supplier/portal/SupplierController.java
+++ torani-cap/torani-supplier/src/main/java/org/coffeehouse/torani/supplier/portal/SupplierController.java
@@ -2,12 +2,15 @@ package org.coffeehouse.torani.supplier.portal;
 
 import org.coffeehouse.torani.core.domain.supplier.Supplier;
 import org.coffeehouse.torani.supplier.service.SupplierSettingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.text.MessageFormat;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,21 +21,14 @@ import java.util.Set;
 @RequestMapping(value = "/suppliers")
 public class SupplierController {
 
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private SupplierSettingService supplierSettingService;
 
-    @RequestMapping
-    @ResponseStatus(HttpStatus.OK)
-    public Set<Supplier> index(){
-        long supplierId = 1;
-        Set<Supplier> suppliers = new HashSet<Supplier>();
-        Supplier s = supplierSettingService.getSupplier(supplierId);
-        suppliers.add(s);
-        return suppliers;
-    }
-
     @RequestMapping(value="/{id}",method= RequestMethod.GET)
     public Supplier get(@PathVariable Long id){
+        logger.info(MessageFormat.format("get id:{0}", id));
         Supplier supplier = supplierSettingService.getSupplier(id);
         return supplier;
     }
